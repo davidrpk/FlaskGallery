0bdf58edce63febab06e474df2a2249b8fe574fe
David Russell-Park <David.Russell-Park@fphcare.co.nz>
2018-12-27 00:49:12 +1300

    Removed all aws deps and simplified requirements

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, redirect, request, flash, escape, session
 from passlib.hash import sha256_crypt
-import boto3
 import json
 
 app = Flask(__name__)
@@ -14,7 +13,7 @@ def read_photos():
     # You can update this dictionary by change the attributes in 'data.json' file. An example of one row is given below
     #[{u'artist': u'John Smith', u'price': u'$250', u'title': u'img 001', u'source': u'../static/img/img.jpg', u'year': u'2012', u'desc': u'description'}]
     with open('data/data.json', 'r') as file:
-        data = json.load(file, 'utf-8')
+        data = json.load(file)
     return data
 
 @app.route('/collection/<objectID>')
@@ -24,7 +23,7 @@ def collectionItem(objectID):
     # This example opens new page and displays the photo with its information
 
     with open('data/data.json', 'r') as file:
-        data = json.load(file, 'utf-8')
+        data = json.load(file)
 
     for record in data:
         if record['objectID'] == objectID:
