650cd96b9fae897a6ebeb2111498d3fd0a8c3114
David Russell-Park <David.Russell-Park@fphcare.co.nz>
2018-12-28 22:38:15 +1300

    Added compliance to PEP 8 spec with Flake8

--- flaskgallery/__init__.py
+++ flaskgallery/__init__.py
@@ -3,6 +3,7 @@ import os
 from flaskgallery.photostorage import SQLitePhotoIndex
 
 app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET','OnceUponATimeThereWasAWickedWitchCalledNodeJS')
+app.secret_key = os.getenv('FLASK_SECRET',
+                           'OnceUponATimeThereWasAWickedWitchCalledNodeJS')
 
 photoindex = SQLitePhotoIndex()
--- flaskgallery/photostorage.py
+++ flaskgallery/photostorage.py
@@ -1,13 +1,17 @@
 import sqlite3
 import json
 
+
 class PhotoIndex:
     def __init__(self):
         print('')
+
     def fetchall(self):
         raise NotImplementedError()
-    def fetchone(self,objectID):
+
+    def fetchone(self, objectID):
         raise NotImplementedError()
+
 
 class SQLitePhotoIndex:
     def fetchall(self):
@@ -15,36 +19,39 @@ class SQLitePhotoIndex:
         db.row_factory = sqlite3.Row
         c = db.cursor()
         c.execute('''SELECT * FROM photos''')
-        rows = c.fetchall()   
+        rows = c.fetchall()
         photos = []
         for row in rows:
             photo = dict(zip(row.keys(), row))
             photos.append(photo)
         db.close()
         return photos
-    def fetchone(self,objectID):
+
+    def fetchone(self, objectID):
         db = sqlite3.connect('data/data.db')
         db.row_factory = sqlite3.Row
         c = db.cursor()
         intObjectID = 0
         try:
             intObjectID = int(objectID)
-        except:
+        except ValueError:
             return None
-        c.execute('''SELECT * FROM photos WHERE objectID=?''',(intObjectID,))
+        c.execute('''SELECT * FROM photos WHERE objectID=?''', (intObjectID,))
         row = c.fetchone()
-        if row != None:
+        if row is not None:
             photo = dict(zip(row.keys(), row))
             return photo
 
         return None
+
 
 class JSONPhotoIndex:
     def fetchall(self):
         with open('data/data.json', 'r') as file:
             data = json.load(file)
         return data
-    def fetchone(self,objectID):
+
+    def fetchone(self, objectID):
         for record in self.fetchall():
             if str(record['objectID']) == str(objectID):
                 return record
