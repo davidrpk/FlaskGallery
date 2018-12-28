89f6ee3baa2c1b3e10495884ba12fc8599a7bac2
David Russell-Park <David.Russell-Park@fphcare.co.nz>
2018-12-29 01:43:49 +1300

    Added endpoint for adding photos

--- flaskgallery/views.py
+++ flaskgallery/views.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, jsonify
 from . import app
 from . import photoindex
-
+import json
 
 @app.route('/')
 def index():
@@ -15,3 +15,13 @@ def collectionItem(objectID):
     if record is not None:
         return render_template('object.html', photo=record)
     return render_template('index.html')
+
+
+@app.route('/api/v1.0/collection/photos', methods=['POST'])
+def add_photo():
+    data = request.get_json()
+    if not data or 'url' not in data['photo']:
+        abort(400)
+    objectID = photoindex.addone(data['photo'])
+    record = photoindex.fetchone(objectID)
+    return jsonify(record), 201
