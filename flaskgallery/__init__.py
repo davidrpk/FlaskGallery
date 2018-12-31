20a53767624d03add497f67d82ff0dfeead57857
David Russell-Park <David.Russell-Park@fphcare.co.nz>
2018-12-31 23:36:07 +1300

    Added connection examples

--- flaskgallery/__init__.py
+++ flaskgallery/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 import os
+from .models.photos_azuretable import PhotoCollectionAzureTable
 from .models.photos_sqlite import PhotoCollectionSQLite
+from .models.photos_jsonfile import PhotoCollectionJSONFile
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET',
                            'OnceUponATimeThereWasAWickedWitchCalledNodeJS')
 
+# SQLite
 photocollection = PhotoCollectionSQLite('data/data.db')
+
+# Azure Table Storage
+# connection_string = os.getenv('STORAGE_CONNECTION_STRING',
+#                              '')
+# photocollection = PhotoCollectionAzureTable(connection_string)
+
+# JSON file
+# photo collection = PhotoCollectionJSONFile('data/data.json')
