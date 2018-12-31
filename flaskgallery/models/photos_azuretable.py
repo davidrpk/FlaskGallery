e145b99dd2c9dff7e210ef1cc946fb96e6ad04b0
David Russell-Park <David.Russell-Park@fphcare.co.nz>
2018-12-31 23:05:37 +1300

    Added remaining methods: fetchall and fetchone

--- flaskgallery/models/photos_azuretable.py
+++ flaskgallery/models/photos_azuretable.py
@@ -4,15 +4,32 @@ from .photocollection import Photo
 
 
 class PhotoCollectionAzureTable:
+    _connectionstring = ''
 
-    def __init__(self):
-        print('')
-  
+    def __init__(self, connectionstring):
+        self._connectionstring = connectionstring
+
+    def fetchall(self):
+        table_service = TableService(connection_string=self._connectionstring)
+        photos = table_service.query_entities('phototable').items
+        [photo.pop('etag', None) for photo in photos]
+        [photo.pop('Timestamp', None) for photo in photos]
+        return photos
+
+    def fetchone(self, objectID):
+        table_service = TableService(connection_string=self._connectionstring)
+        photos = table_service.query_entities('phototable',
+                                              "RowKey eq '" + objectID + "'"
+                                              ).items
+        [photo.pop('etag', None) for photo in photos]
+        [photo.pop('Timestamp', None) for photo in photos]
+        if photos:
+            return photos[0]
+        return None
+
     def addone(self, photo):
-        #this expects photo to be an object not a json string
-        table_service = TableService(connection_string="")
-        photo_dict = vars(photo)
-        photo_dict['PartitionKey'] = photo.taken
-        photo_dict['RowKey'] = photo.CRC
-        print(photo_dict)
-        table_service.insert_entity('phototable', photo_dict)
+        table_service = TableService(connection_string=self._connectionstring)
+        photoAzure = photo
+        photoAzure['PartitionKey'] = photo['taken']
+        photoAzure['RowKey'] = photo['objectID']
+        table_service.insert_entity('phototable', photoAzure)
